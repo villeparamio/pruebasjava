9dbf933e3c2a6b5e250ab39318d1f9289efe7a31
villeparamio <david@camposgoticos.com>
2017-10-24 11:14:51 +0200

    archivos añadidos

---
+++ ej26.java
@@ -0,0 +1,23 @@
+// Ejercicio 26
+// Plantilla para resolución de ejercicios
+/*
+
+	Entradas: cantidad de euros de tipo entero
+	Salidas: No tiene salida (salida lateral = pantalla)
+	PRE (precondiciones): c >= 0; La cantidad debe ser entera y positiva
+
+		Ejemplo de uso: 
+
+			main () {
+				int p= 3452;
+				escribirDescompueto(p);
+			}
+
+
+*/
+
+public class ej26 {
+	public static void escribirDescompueto (int numero) {
+		System.out.println()
+	}
+}
---
+++ ej27.java
@@ -0,0 +1,12 @@
+// Ejercicio 27
+
+/*
+	Entradas: numero de segundos int
+	Salidas: lateral
+	PRE: n >= 0; n <= 60*60*24 
+
+*/
+
+public static double redondeoA2 (double n){
+	
+}

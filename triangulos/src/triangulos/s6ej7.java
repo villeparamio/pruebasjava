072fe6c70c325e1e6d92e1afb4865727ae83522a
davpara <davpara@l103a5-linux.lab.inf.uva.es>
2017-11-16 13:59:43 +0100

    triangulocentral

---
+++ triangulos/src/triangulos/s6ej7.java
@@ -0,0 +1,14 @@
+package triangulos;
+
+import java.util.Scanner;
+
+public class s6ej7 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+	    int n;
+	    System.out.println ("Teclee un número: ");
+	    n = in.nextInt();
+	}
+
+}

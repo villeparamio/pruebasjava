dfef06b0ab3dd8e48a1b24f98a2fd4e275c366e0
villeparamio <david@camposgoticos.com>
2017-11-20 13:35:47 +0100

    ejercicio 1 completado

--- sesionEvaluacionV1/src/sesionEvaluacionV1/enunciado1.java
+++ sesionEvaluacionV1/src/sesionEvaluacionV1/enunciado1.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class enunciado1 {
 	
-	// Declaramos la función que actúa sobre los números pares
-	
-	public static long par (long n){
-		n = n / 2;
-		return n;
-    }
-	
-	// Declaramos la función que actúa sobre los números impares
-	
-	public static long impar (long n){
-		n = n*3+1;
-		return n;
+	// Declaramos la función que calcula la sucesión
+
+    public static long sucesion (long n) {
+    	long sucesion = 1;
+    	while (n > 1){
+    		if (n%2 == 0) {
+			n = n / 2;
+			} else {
+	  			n = n*3+1;
+			}
+			sucesion++;
+    	}
+    	return sucesion;
     }
 
 	public static void main(String[] args) {
@@ -31,57 +32,25 @@ public class enunciado1 {
 	      n2 = in.nextInt();
 	      
 	      // colocamos el número más alto primero
-	      long longitud = 0;
-	      long distancia = 0;
+	      long longitudMax = 0;
 	      if (n1 > n2){
 	    	  for (long contador = n2;n1>contador;contador++){
-	    		  while (contador>1) {
-			    	  if (contador%2 == 0){
-			    		  contador = par(contador);
-			    	  }else {
-			    		  contador = impar(contador);
-			    	  }
+		    	  long sucesion = sucesion(contador);
+		    	  if (sucesion > longitudMax) {
+		    	  	longitudMax = sucesion;
 		    	  }
-	    		  distancia++;
 	    	  }
 	      } else if (n1 < n2) {
-	    	  
-	      } else {
-		      while (n1>1) {
-		    	  
-		    	  if (n1%2 == 0){
-		    		  n1 = par(n1);
-		    	  }else {
-		    		  n1 = impar(n1);
+	    	  for (long contador = n1;n2>contador;contador++){
+		    	  long sucesion = sucesion(contador);
+		    	  if (sucesion > longitudMax) {
+		    	  	longitudMax = sucesion;
 		    	  }
-		    	  longitud ++;	    	 
-		      } 
-	      }
-	      
-	      
-	      
-	      /*// Operamos con el segundo número
-	      
-	      long longitudN2 = 0;
-	      while (n2>1) {
-	    	  if (n2%2 == 0){
-	    		  n2 = par(n2);
-	    	  }else {
-	    		  n2 = impar(n2);
 	    	  }
-	    	  longitudN2 ++;	    	 
-	      }*/
-	      
-	      // Sumamos uno para contar con el propio número en la sucesión
-	      //longitudN1 = longitudN1 + 1;
-	      //longitudN2 = longitudN2 + 1;
-
-	      
-	      System.out.println("distancia: " + distancia);
-	      /*
-	      System.out.println(n1 + " 1");
-	      System.out.println(n2 + " 2");*/
-
+	      } else {
+		      longitudMax = sucesion(n1);
+	      }	      
+	      System.out.println("La longitud máxima de la cadena es: " + longitudMax);
 	}
 
 }
